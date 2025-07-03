15c3e64d4b8e26a40829f4b8b16b68120c7e83e2
Sangeetha M <sangeecharu0001@gmail.com>
2025-07-03 22:02:33 +0530

    fix: bind to 0.0.0.0 and use Render port

--- app.py
+++ app.py
@@ -82,4 +82,4 @@ def update_dashboard(products, regions, start_date, end_date):
 # Run app
     if __name__ == '__main__':
         port=int(os.environ.get("PORT",8050))
-        app.run(host='0.0.0.0',port=port,debug=True)
+        app.run(host="0.0.0.0",port=port,debug=True)
